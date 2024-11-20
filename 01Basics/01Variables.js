daf45aa13556e08ada07e6dc813e55fd629a98a3
Shivam Sahu <104662811+ShivamSahu2001@users.noreply.github.com>
2024-11-20 16:44:17 +0000

    variables in js

---
+++ 01Basics/01Variables.js
@@ -0,0 +1,14 @@
+const accountId = 343423;
+let accountEmail = "shivam@gmail.com";
+var accountPassword = "234343";
+accountCity = "Amethi";
+let accountState;
+
+// accountId = 34343;   //not allowed 
+
+accountEmail = "sahu@gmail.com"
+accountPassword = "5343343";
+accountCity = "Hyderabad";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
