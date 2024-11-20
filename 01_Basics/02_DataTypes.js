6a6a92928fce1f9c15f5809fd9c0503358ea20c0
Shivam Sahu <104662811+ShivamSahu2001@users.noreply.github.com>
2024-11-20 17:17:29 +0000

    DataTypes in JS

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict";  // treat all JS Code as newer version
+
+let name = "shivam";
+let age = 18
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint 
+// String => ""
+// boolean => true/falase
+// null => standalone value
+// undefind =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); //object
